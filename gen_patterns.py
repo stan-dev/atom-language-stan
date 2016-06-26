d4b5cd813df6302f4f2de5bd631581c2f2a55b13
jrnold <jeffrey.arnold@gmail.com>
2016-06-26 12:40:09 -0700

    Fix gen_patterns
    The format of stan_lang.json changed; the 'distributions' key was removed

--- gen_patterns.py
+++ gen_patterns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-""" 
+"""
 Create the regexes to use in the Atom language-stan grammar
 
 This is a hack until I translate it into coffescript script that directly outputs the .cson grammar.
@@ -20,6 +20,10 @@ def patterns(filename):
         for x in data['types'][k]:
             types.add(x)
     types = sorted(list(types))
+
+    distributions = [re.sub(r'_p(dm)f$', '', x)
+                     for x in data['functions']['names']['density']]
+
     print("types: " + r'\\b' + kw2re(types) + r'\\b')
     print("blocks: " + r'\\b' + kw2re(re.sub(r'\s+', r'\\\\s+', x) for x in data['blocks']) + r'\\b')
     print("keywords-range-constraings: " + r'\\b' + kw2re(data['keywords']['range_constraints']) + r'\\b')
@@ -29,7 +33,7 @@ def patterns(filename):
     print("cpp-reserved: " + r'\\b' + kw2re(data['reserved']['cpp']) + r'\\b')
     print("stan-reserved: " + r'\\b' + kw2re(data['reserved']['stan']) + r'\\b')
     print("functions: " + r'\\b' + kw2re(sorted(data['functions']['names']['all'])) + r'\\b')
-    print("distributions: " + r'\\b(~)\\s*' + kw2re(sorted(data['distributions'])) + r'\\b')
+    print("distributions: " + r'\\b(~)\\s*' + kw2re(distributions) + r'\\b')
     operators = []
     for x in sorted(data['operators'], key = len, reverse = True):
         if x == '\\':
@@ -38,10 +42,10 @@ def patterns(filename):
             x = r'\\^'
         else:
             x = ''.join('[%s]' % el for el in x)
-            
+
         operators.append(x)
     print("operators: " + r'\\b' + kw2re(operators) + r'\\b')
-    
+
 
 def main():
     src = 'stan-language-definitions/stan_lang.json'
@@ -49,4 +53,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
